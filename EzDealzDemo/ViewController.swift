d546f726451ca6be50361b97469894a008dd0a57
Muhammad Khan <fraz@entertechsolutions.com>
2018-04-25 19:56:07 +0500

    initial commit

--- EzDealzDemo/ViewController.swift
+++ EzDealzDemo/ViewController.swift
@@ -7,19 +7,67 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
+    
+    let loginURL = "http://www.ezdealz.com/main/doLogin"
+    let signupURL = "http://www.ezdealz.com/main/doSignup"
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        login()
+        signup()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    private func login() {
+        
+        let params: Parameters = [ "email" : "mk@gmail.com",
+                                   "password" : "123456",
+                                   "app_type" : "ios",
+                                   "app_token" : "1234567890" ]
+        
+        Alamofire.request(loginURL, method: .post, parameters: params, encoding: JSONEncoding.default, headers: nil).response { (response) in
+            if let error = response.error {
+                print("Network Error: ", error)
+            } else {
+                do {
+                    let json = try JSONSerialization.jsonObject(with: response.data!, options: .allowFragments) as! [String: AnyObject]
+                    print(json)
+                } catch {
+                    print("Catch Error: ", error)
+                }
+            }
+        }
     }
-
-
+    
+    private func signup() {
+        
+        let params: Parameters = [ "fname" : "MK",
+                                   "email" : "mk@gmail.com",
+                                   "password" : "123456",
+                                   "phone" : "03338222120",
+                                   "address" : "Gulistan e Johar",
+                                   "city" : "Karachi",
+                                   "dob" : "1234",
+                                   "gender" : "male",
+                                   "app_token" : "1234567890",
+                                   "app_type" : "ios" ]
+        
+        Alamofire.request(signupURL, method: .post, parameters: params, encoding: JSONEncoding.default, headers: nil).response { (response) in
+            if let error = response.error {
+                print("Network Error: ", error)
+            } else {
+                do {
+                    let json = try JSONSerialization.jsonObject(with: response.data!, options: .allowFragments) as! [String: AnyObject]
+                    print(json)
+                } catch {
+                    print("Catch Error: ", error)
+                }
+            }
+        }
+    }
+    
 }
 
---
+++ Pods/Target Support Files/Pods-EzDealzDemo/Pods-EzDealzDemo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_EzDealzDemo : NSObject
+@end
+@implementation PodsDummy_Pods_EzDealzDemo
+@end
